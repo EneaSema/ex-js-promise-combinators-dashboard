2b77c8892a7c4043422effd1566a204cbab35bd1
EneaSema <sema.enea1993@gmail.com>
2025-08-29 14:54:11 +0200

    resolve snack5 bonus2 and corrects errors and integrations solutions

--- snack5.js
+++ snack5.js
@@ -41,7 +41,10 @@ console.log(`ciao`);
 // Today there are 18 degrees and the weather is Partly cloudy.
 // The main airport is London Heathrow Airport.
 
+//SOLUZIONE:
+
 // async function getDashboardData(query) {
+//try{
 //   const destinationsFetch = await fetch(
 //     `http://localhost:3333/destinations?search=${[query]}`
 //   );
@@ -86,6 +89,10 @@ console.log(`ciao`);
 //   };
 //   console.log(data);
 //   return data;
+// } catch (error) {
+//   console.log("Errore nel recuepro dei dati;", error.messagge)
+// }
+
 // }
 
 // (async () => {
@@ -106,6 +113,84 @@ console.log(`ciao`);
 // semplicemente i dati relativi a quella chiamata verranno settati a null e  la frase relativa non viene stampata.
 // Testa la funzione con la query “vienna” (non trova il meteo).
 
+// async function getDashboardData(query) {
+//   const destinationsFetch = await fetch(
+//     `http://localhost:3333/destinations?search=${[query]}`
+//   );
+//   const destinationsResponse = destinationsFetch.json();
+//   console.log(destinationsResponse);
+
+//   const weathersFetch = await fetch(
+//     `http://localhost:3333/weathers?search=${query}`
+//   );
+//   const weathersResponse = weathersFetch.json();
+//   console.log(weathersResponse);
+
+//   const airportsFetch = await fetch(
+//     `http://localhost:3333/airports?search=${query}`
+//   );
+//   const airportsResponse = airportsFetch.json();
+//   console.log(airportsResponse);
+
+//   const promises = [destinationsResponse, weathersResponse, airportsResponse];
+
+//   const results = await Promise.all(promises);
+//   console.log(results);
+
+//   const [destinations, weathers, airports] = results;
+//   console.log(`Risultati di promise.all:`, results);
+
+//   const data = {
+//     city: null,
+//     country: null,
+//     temperature: null,
+//     weather: null,
+//     airport: null,
+//   };
+
+//   if (destinations.length > 0) {
+//     data.city = destinations[0].name;
+//     data.country = destinations[0].country;
+//   }
+
+//   if (weathers.length > 0) {
+//     data.temperature = weathers[0].temperature;
+//     data.weather = weathers[0].weathers_description;
+//   }
+
+//   if (airports.length > 0) {
+//     data.airport = airports[0].name;
+//   }
+
+//   console.log(data);
+//   return data;
+// }
+
+// (async () => {
+//   getDashboardData("vienna")
+//     .then((data) => {
+//       console.log("Dasboard data:", data);
+//       let outString = `${data.city} is in ${data.country}.\n`;
+
+//       if (data.temperature !== null) {
+//         outString += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`;
+//       }
+//       if (data.airport !== null)
+//         [(outString += `The main airport is ${data.airport}.\n`)];
+
+//       console.log(outString);
+//     })
+//     .catch((error) => console.error(error));
+// })();
+
+//🎯 Bonus 2 - Chiamate fallite
+//Attualmente, se una delle chiamate fallisce, **Promise.all()** rigetta l'intera operazione.
+
+//Modifica `getDashboardData()` per usare **Promise.allSettled()**, in modo che:
+//Se una chiamata fallisce, i dati relativi a quella chiamata verranno settati a null.
+//Stampa in console un messaggio di errore per ogni richiesta fallita.
+//Testa la funzione con un link fittizio per il meteo (es. https://www.meteofittizio.it).
+
 async function getDashboardData(query) {
   const destinationsFetch = await fetch(
     `http://localhost:3333/destinations?search=${[query]}`
@@ -113,9 +198,7 @@ async function getDashboardData(query) {
   const destinationsResponse = destinationsFetch.json();
   console.log(destinationsResponse);
 
-  const weathersFetch = await fetch(
-    `http://localhost:3333/weathers?search=${query}`
-  );
+  const weathersFetch = await fetch(`https://www.meteofittizio.it`);
   const weathersResponse = weathersFetch.json();
   console.log(weathersResponse);
 
@@ -127,10 +210,8 @@ async function getDashboardData(query) {
 
   const promises = [destinationsResponse, weathersResponse, airportsResponse];
 
-  const results = await Promise.all(promises);
-  console.log(results);
-
-  const [destinations, weathers, airports] = results;
+  const results = await Promise.allSettled(promises);
+  console.log(`Risultati di promise.allSettled:`, results);
 
   const data = {
     city: null,
@@ -140,18 +221,32 @@ async function getDashboardData(query) {
     airport: null,
   };
 
-  if (destinations.length > 0) {
-    data.city = destinations[0].name;
-    data.country = destinations[0].country;
+  const [destinations, weathers, airports] = results;
+
+  if (results[0].status === `fulfilled` && results[0].value.length > 0) {
+    data.city = results[0].value[0].name;
+    data.country = results[0].value[0].country;
+  } else if (results[0].status === `rejected`) {
+    console.error(
+      "Errore nella richiesta per le destinazioni:",
+      results[0].reason
+    );
   }
 
-  if (weathers.length > 0) {
-    data.temperature = weathers[0].temperature;
-    data.weather = weathers[0].weathers_description;
+  if (results.status === `fulfilled` && results[1].value[0].length > 0) {
+    data.temperature = results[1].value[0].temperature;
+    data.weather = results[1].value[0].weathers_description;
+  } else if (results[1].status === `rejected`) {
+    console.error("Errore nella richiesta per il meteo:", results[1].reason);
   }
 
-  if (airports.length > 0) {
-    data.airport = airports[0].name;
+  if (results.status === `fulfilled` && results[2].value[0].length > 0) {
+    data.airport = results[2][1].name;
+  } else if (results[2].status === `rejected`) {
+    console.error(
+      "Errore nella richiesta per li aereporti:",
+      results[2].reason
+    );
   }
 
   console.log(data);
@@ -159,18 +254,25 @@ async function getDashboardData(query) {
 }
 
 (async () => {
-  getDashboardData("vienna")
-    .then((data) => {
-      console.log("Dasboard data:", data);
-      let outString = `${data.city} is in ${data.country}.\n`;
+  try {
+    const data = await getDashboardData("vienna");
 
-      if (data.temperature !== null) {
-        outString += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`;
-      }
-      if (data.airport !== null)
-        [(outString += `The main airport is ${data.airport}.\n`)];
+    console.log("Dasboard data:", data);
 
-      console.log(outString);
-    })
-    .catch((error) => console.error(error));
+    let outputString = "";
+
+    if (data.city !== null && data.country !== null) {
+      outputString += `${data.city} is in ${data.country}.\n`;
+    }
+
+    if (data.temperature !== null && data.weather !== null) {
+      outString += `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`;
+    }
+    if (data.airport !== null)
+      [(outString += `The main airport is ${data.airport}.\n`)];
+
+    console.log(outString);
+  } catch (error) {
+    console.error("Si è verificato un errore:", error);
+  }
 })();
